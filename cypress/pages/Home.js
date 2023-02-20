7538cb4f3aa2f7ab29688d81fb182ba1c8c06317
Bruna F <brunarodrigues0899@gmail.com>
2023-02-20 17:41:52 -0300

    ok

--- cypress/pages/Home.js
+++ cypress/pages/Home.js
@@ -1,13 +1,19 @@
 const mainTitle = '[class="sc-gswNZR CUaRA sc-jSUZER esTqtV"]'
+const inputHeroSearch = '[name="heroSearch"]'
 
 class Home {
 
   go() {
     cy.visit('/');
-    cy.get(mainTitle).should('have.text', 'Explore o universo')
+    cy.get(mainTitle).then(($el) => {
+      expect($el).to.have.text("Explore o universo")
+    });
   }
 
-  
+  typeHeroSearch(data) {
+    cy.get(inputHeroSearch).clear().type(data.name)
+  }
+
 }
 
 export { Home }
