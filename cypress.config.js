e720f5a6c842970197b8b5357b974334f8653b69
Bruna F <brunarodrigues0899@gmail.com>
2023-02-21 18:11:20 -0300

    Projeto Cypress Finalizado

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ chromeWebSecurity: true,
   viewportHeight: 768,
   projectId: "au9tap",
   e2e: {
-    baseUrl: 'https://chrigor-marvel-hero-app.netlify.app/',
+    baseUrl: 'http://localhost:3000/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 });
